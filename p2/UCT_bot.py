a7fe25d6e66f34464ef40d7f842ba533dc2e9c60
Max Savage <msavage@ucsc.edu>
2015-04-12 14:57:41 -0700

    greedy_bot works, turned off greedy_bot printout

---
+++ p2/UCT_bot.py
@@ -0,0 +1,21 @@
+import time
+
+THINK_DURATION = 1
+
+def think(state, quip):
+
+    t_start = time.time()
+    t_deadline = t_start + THINK_DURATION
+
+    iterations = 0
+
+    while True:
+
+        iterations += 1
+        # select, expand, rollout, then backpropagate here
+
+        t_now = time.time()
+        if t_now > t_deadline:
+            break
+
+    sample_rate = float(iterations)/(t_now - t_start)
