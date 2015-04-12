d4213e91a18c6b1c51f5b1a85093badad06ef558
Max Savage <msavage@ucsc.edu>
2015-04-12 13:17:20 -0700

    no message

--- P1.py
+++ P1.py
@@ -3,21 +3,10 @@ from math import sqrt
 from heapq import heappush, heappop
 
 def dijkstras_shortest_path(src, dst, graph, adj):
-  visited = {initial: 0}
+	raise NotImplementedError
 
 def navigation_edges(level, cell):
-  def __init__(self):
-	self.nodes = set()
-    self.edges = defaultdict(list)
-    self.distances = {}
- 
-  def add_node(self, value):
-    self.nodes.add(value)
- 
-  def add_edge(self, from_node, to_node, distance):
-    self.edges[from_node].append(to_node)
-    self.edges[to_node].append(from_node)
-    self.distances[(from_node, to_node)] = distance
+	raise NotImplementedError
 
 def test_route(filename, src_waypoint, dst_waypoint):
 	level = load_level(filename)
